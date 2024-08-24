d647591f505db309dabce701568b23f7babccd63
Dowon Lee <ldwlee03@gmail.com>
2024-08-24 16:47:54 +0900

    update java basic

--- src/ch01/src/sec10/Hello.java
+++
@@ -1,9 +0,0 @@
-package ch01.src.sec10;
-
-public class Hello {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, Java");
-    }
-
-}
---
+++ src/ch02/sec02/ByteExample.java
@@ -0,0 +1,12 @@
+package ch02.sec02;
+
+public class ByteExample {
+    public static void main(String[] args) {
+        byte var1 = -128;
+        byte var2 = 127;
+
+        System.out.println(var1);
+        System.out.println(var2);
+
+    }
+}
