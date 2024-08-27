80130e5ff2c53705f37e71341dbb1b580469a375
Dowon Lee <ldwlee03@gmail.com>
2024-08-27 15:33:58 +0900

    update

---
+++ src/ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        int[] arr1 = null;
+        //arr1[0] = 10;
+
+        String str = null;
+        //System.out.println("length : " + str.length());
+    }
+}
